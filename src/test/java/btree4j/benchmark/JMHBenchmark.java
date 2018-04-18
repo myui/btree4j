db00ce656cbb49eca4f4d8a78c24833f46844d15
Makoto Yui <myui@apache.org>
2018-04-18 16:49:10 +0900

    Fixed JMH bench

--- src/test/java/btree4j/benchmark/JMHBenchmark.java
+++ src/test/java/btree4j/benchmark/JMHBenchmark.java
@@ -15,20 +15,102 @@
  */
 package btree4j.benchmark;
 
+import btree4j.BIndexFile;
+import btree4j.BTreeException;
+import btree4j.Value;
+import btree4j.utils.io.FileUtils;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OperationsPerInvocation;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
-public final class JMHBenchmark {
+@State(Scope.Thread)
+public class JMHBenchmark {
+
+    private BIndexFile btree;
+
+    @Setup
+    public void setup() throws BTreeException {
+        File tmpDir = FileUtils.getTempDir();
+        Assert.assertTrue(tmpDir.exists());
+        File tmpFile = new File(tmpDir, "JMHBenchmark.idx");
+        tmpFile.deleteOnExit();
+        if (tmpFile.exists()) {
+            Assert.assertTrue(tmpFile.delete());
+        }
+        this.btree = new BIndexFile(tmpFile);
+        btree.init(/* bulkload */ false);
+    }
+
+    @TearDown
+    public void tearDown() throws BTreeException {
+        btree.close();
+    }
+
+    //
+    // JMHBenchmark.benchAdd1k       thrpt   10   98079.705 ± 9358.808  ops/s    
+    //
+    @Benchmark
+    @OperationsPerInvocation(1000)
+    public void benchAdd1k() throws BTreeException {
+        runAdd(1000);
+    }
+
+    //
+    // JMHBenchmark.benchAdd1kGet1k  thrpt   10  146025.716 ± 7516.560  ops/s
+    //
+    @Benchmark
+    @OperationsPerInvocation(2000)
+    public void benchAdd1kGet1k() throws BTreeException {
+        runAdd(1000);
+        runGet(1000);
+    }
+
+    public void runAdd(final int count) throws BTreeException {
+        for (int i = 0; i < count; i++) {
+            Value k = new Value("k" + i);
+            Value v = new Value("v" + i);
+
+            btree.addValue(k, v);
+            if (i % 100 == 0) {
+                btree.putValue(k, new Value("v" + i + "_u"));
+            }
+        }
+    }
+
+    public void runGet(final int count) throws BTreeException {
+        for (int i = 0; i < count; i++) {
+            Value k = new Value("k" + i);
+            final Value expected;
+            if (i % 100 == 0) {
+                expected = new Value("v" + i + "_u");
+            } else {
+                expected = new Value("v" + i);
+            }
+            Value actual = btree.getValue(k);
+            Assert.assertEquals(expected, actual);
+        }
+    }
 
     public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(JMHBenchmark.class.getSimpleName())
-                .forks(1)
-                .build();
+        Options opt = new OptionsBuilder().include(JMHBenchmark.class.getSimpleName())
+                                          .forks(1)
+                                          .warmupIterations(5)
+                                          .measurementIterations(10)
+                                          .mode(Mode.Throughput)
+                                          .build();
 
         new Runner(opt).run();
     }
-
 }
