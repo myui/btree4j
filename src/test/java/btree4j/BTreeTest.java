55c9aa07965245db4401a52b23781f8ecb466c8c
Abtin Bateni <abtinbateni@gmail.com>
2019-05-05 18:06:22 +0430

    Add/refactor tests

--- src/test/java/btree4j/BTreeTest.java
+++ src/test/java/btree4j/BTreeTest.java
@@ -28,11 +28,14 @@ import java.util.Random;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static btree4j.BTree.KEY_NOT_FOUND;
+
 public class BTreeTest {
     private static final boolean DEBUG = true;
+    final private int MAXN = 5000 * 1000;
 
     @Test
-    public void test() throws BTreeException {
+    public void shouldAdd5m() throws BTreeException {
         File tmpDir = FileUtils.getTempDir();
         Assert.assertTrue(tmpDir.exists());
         File tmpFile = new File(tmpDir, "BTreeTest1.idx");
@@ -44,37 +47,83 @@ public class BTreeTest {
         BTree btree = new BTree(tmpFile);
         btree.init(/* bulkload */ false);
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < MAXN; i++) {
             Value k = new Value("k" + i);
-            long v = i;
-            btree.addValue(k, v);
+            btree.addValue(k, i);
         }
 
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < MAXN; i++) {
             Value k = new Value("k" + i);
-            long expected = i;
             long actual = btree.findValue(k);
-            Assert.assertEquals(expected, actual);
+            Assert.assertEquals(i, actual);
         }
-
-        btree.search(new IndexConditionBW(new Value("k" + 900), new Value("k" + 910)),
-            new BTreeCallback() {
-
-                @Override
-                public boolean indexInfo(Value value, long pointer) {
-                    //System.out.println(pointer);
-                    return true;
-                }
-
-                @Override
-                public boolean indexInfo(Value key, byte[] value) {
-                    throw new UnsupportedOperationException();
-                }
-            });
     }
 
     @Test
-    public void test10m() throws BTreeException {
+    public void shouldAddRemove5m() throws BTreeException {
+        File tmpDir = FileUtils.getTempDir();
+        Assert.assertTrue(tmpDir.exists());
+        File tmpFile = new File(tmpDir, "BTreeTest1.idx");
+        tmpFile.deleteOnExit();
+        if (tmpFile.exists()) {
+            Assert.assertTrue(tmpFile.delete());
+        }
+
+        BTree btree = new BTree(tmpFile);
+        btree.init(/* bulkload */ false);
+
+        for (int i = 0; i < MAXN; i++) {
+            Value k = new Value("k" + i);
+            btree.addValue(k, i);
+        }
+
+        for (int i = 0; i < MAXN; i++) {
+            Value k = new Value("k" + i);
+            btree.removeValue(k);
+        }
+
+        for (int i = 0; i < MAXN; i++) {
+            Value k = new Value("k" + i);
+            long actual = btree.findValue(k);
+            Assert.assertEquals(KEY_NOT_FOUND, actual);
+        }
+    }
+
+    @Test
+    public void shouldAddThenRemoveHalf5m() throws BTreeException {
+        File tmpDir = FileUtils.getTempDir();
+        Assert.assertTrue(tmpDir.exists());
+        File tmpFile = new File(tmpDir, "BTreeTest1.idx");
+        tmpFile.deleteOnExit();
+        if (tmpFile.exists()) {
+            Assert.assertTrue(tmpFile.delete());
+        }
+
+        BTree btree = new BTree(tmpFile);
+        btree.init(/* bulkload */ false);
+
+        for (int i = 0; i < MAXN; i++) {
+            Value k = new Value("k" + i);
+            btree.addValue(k, i);
+        }
+
+        for (int i = 0; i < MAXN/2; i++) {
+            Value k = new Value("k" + i);
+            btree.removeValue(k);
+        }
+
+        for (int i = 0; i < MAXN; i++) {
+            Value k = new Value("k" + i);
+            long actual = btree.findValue(k);
+            if (i < MAXN / 2)
+                Assert.assertEquals(KEY_NOT_FOUND, actual);
+            else
+                Assert.assertEquals(i, actual);
+        }
+    }
+
+    @Test
+    public void shouldAddRemoveRandom10m() throws BTreeException {
         File tmpDir = FileUtils.getTempDir();
         Assert.assertTrue(tmpDir.exists());
         File indexFile = new File(tmpDir, "test10m.idx");
@@ -88,7 +137,7 @@ public class BTreeTest {
 
         final Map<Value, Long> kv = new HashMap<>();
         final Random rand = new Random();
-        for (int i = 0; i < 10000000; i++) {
+        for (int i = 0; i < 2 * MAXN; i++) {
             long nt = System.nanoTime(), val = rand.nextInt(Integer.MAX_VALUE); // FIXME val = rand.nextLong();
             Value key = new Value(String.valueOf(nt) + val);
             btree.addValue(key, val);
@@ -115,9 +164,9 @@ public class BTreeTest {
             Value k = e.getKey();
             Long v = e.getValue();
             long result = btree.findValue(k);
-            Assert.assertNotEquals("key is not registered: " + k, BTree.KEY_NOT_FOUND, result);
+            Assert.assertNotEquals("key is not registered: " + k, KEY_NOT_FOUND, result);
             Assert.assertEquals("Exexpected value '" + result + "' found for key: " + k,
-                v.longValue(), result);
+                    v.longValue(), result);
         }
     }
 
