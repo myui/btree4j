a966126c5c552a0b1c35303e287335db9f98349b
Abtin Bateni <abtinbateni@gmail.com>
2019-05-05 17:16:54 +0430

    Add resetDataLength

--- src/main/java/btree4j/BTree.java
+++ src/main/java/btree4j/BTree.java
@@ -50,6 +50,7 @@ import java.util.Arrays;
 
 import javax.annotation.CheckForNull;
 
+import com.google.common.annotations.Beta;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
@@ -929,12 +930,12 @@ public class BTree extends Paged {
             this.ptrs = ptrs;
             this.ph.setValueCount((short) vlen);
             if (vlen > 1) {
-                final int prevPreixLen = ph.getPrefixLength();
+                final int prevPrefixLen = ph.getPrefixLength();
                 this.prefix = getPrefix(values[0], values[vlen - 1]);
                 final int prefixLen = prefix.getLength();
                 assert (prefixLen <= Short.MAX_VALUE) : prefixLen;
-                if (prefixLen != prevPreixLen) {
-                    int diff = prefixLen - prevPreixLen;
+                if (prefixLen != prevPrefixLen) {
+                    int diff = prefixLen - prevPrefixLen;
                     currentDataLen += diff;
                     ph.setPrefixLength((short) prefixLen);
                 }
@@ -1061,6 +1062,10 @@ public class BTree extends Paged {
             writeValue(page, new Value(bos.toByteArray()));
             this.parentCache = null;
             setDirty(false);
+        }
+
+        private void resetDataLength() {
+            currentDataLen = -1;
         }
 
         private int calculateDataLength() {
